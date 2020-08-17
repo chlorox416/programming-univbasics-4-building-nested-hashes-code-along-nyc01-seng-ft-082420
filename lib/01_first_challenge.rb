393335f35a3a0ff03783e6d170eef937673aa949
C. Chang <chloechang416@gmail.com>
2020-08-17 03:51:19 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -7,9 +7,9 @@ def first_challenge
        :hero_friends => [],
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
+       :patriarch => {name: "Lord Capulet", age: "50"},
+       :matriarch => {name: "Lady Capulet", age: "51"},
+       :heroine => {name: "Juliet", age: "15", status: "alive"},
        :hero_friends => [],
      }
    }
