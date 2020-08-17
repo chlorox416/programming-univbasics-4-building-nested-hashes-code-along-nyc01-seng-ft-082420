a19f7a23212c4a615fc7de4f101d92338610b084
C. Chang <chloechang416@gmail.com>
2020-08-17 03:49:53 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,16 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {},
-     :capulet => {}
+     :montague => {
+       :patriarch => {name: "Lord Montague", age: "53"},
+       :matriarch => {name: "Lady Montague", age: "54"},
+       :hero => {name: "Romeo", age: "15", status: "alive"},
+       :hero_friends => [],
+     },
+     :capulet => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => [],
+     }
    }
 end
